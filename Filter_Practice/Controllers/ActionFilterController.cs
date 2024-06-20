c357ddc9922d7ecabf64faaf51418d784dc72383
Sanjay Tiwari <Sanjutiwari9559@gmail.com>
2024-06-20 16:20:27 +0530

    first commit

---
+++ Filter_Practice/Controllers/ActionFilterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace Filter_Practice.Controllers
+{
+
+    public class ActionFilterController : Controller
+    {
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Server, NoStore = false)]
+        public IActionResult Index()
+        {
+            Response.Headers["Cache-Control"] = "public, max-age=60";
+            return View();
+        }
+    }
+}
